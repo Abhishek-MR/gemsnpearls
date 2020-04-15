c263161e5f06de9441f75f9ee032dfd2c92a352b
Basith Kunimal <basith@intellicar.in>
2020-04-15 18:48:48 +0530

    initial

---
+++ src/pages/index.js
@@ -0,0 +1,108 @@
+import React from 'react'
+
+export default () => (
+  <div className="Home">
+    <h1>Meet Gems n Pearls</h1>
+    <div className="sect">
+      Gems ‘n’ Pearls is an educational hub established in the year 2011 facilitating
+      empowerment of individuals across age groups starting as early as 1.8years to any age through
+      various educational segments. Under the brand name Gems ‘n’ Pearls we run a Preschool in
+      Ajmera, the Area franchisee of Helen O’Grady International Edudrama Academy in Pimpri-
+      Chinchwad , Franchisee for Chrysaalis i-Maths “Intelligent Maths” at Ajmera and Pradhikaran,
+      franchisee for Early Child Care Education – preschool teacher training program, Corporate Training,
+      and Spoken English for adults. Gems ‘n’ Pearls Academy is moving into its 10 th Academic year with
+      2020-21
+    </div>
+    <h1>Our philosophy</h1>
+    <div className="sect">
+      Research states 90% of the brain develops by the age of 5 years and a child shapes
+      his or her beliefs, values and life in these primitive years. At Gems ’n’ Pearls preschool, we provide a
+      safe, nurturing and stimulating environment for all children. We believe in allowing children to
+      express themselves through language, art, creativity and physical activity, by providing an
+      environment that is challenging and fun. We trust experiential learning is deemed to empower
+      children and hence facilitating an environment that welcomes every child into its system and allows
+      them to explore, have fun and learn or imbibe life skills is our philosophy.
+    </div>
+    <h1>Academic Team</h1>
+    <div className="sect">
+      Our teachers are dedicated, committed, energetic and passionate for education.
+      They set the stage for a lifelong learning and encourage the development of positive skills that
+      children master as they grow. Our teachers are a qualified team of professionals who participate in
+      ongoing development activities and workshops to update with the educational trends in accordance
+      to the changing needs of modern children and families.
+    </div>
+    <h1>Founder &amp; Director</h1>
+    <div className="sect">
+      R. Jayaprabha, a Management Professional in HR, with a wide experience as
+      the Head, Dept of Management ; HR in the Industry; Visiting Faculty for IGNOU MCA , M.Ed. for over
+      a period of 8 years and an Edupreneur for the past 9 years with a desire to build an educational
+      system that empowers an individual in imbibing with the traits needed for Life. Has trained
+      individuals across age groups: MBA, MCA, MEd, Hotel Management, Teachers, Corporates, women,
+      high school, primary and pre-school kids. Is passionate about education, believes education
+      empowers and desires to empower women and kids the future of the world.
+    </div>
+    <h1>Amenities</h1>
+    <div className="sect">
+      <ul>
+        <li>Good teacher: Student Ratio</li>
+        <li>Experienced &amp; Qualified Staff</li>
+        <li>Visual Learners area -Smart board &amp; Projector</li>
+        <li>Theme Concept Teaching</li>
+        <li>Development Learning Practises</li>
+        <li>Aquarium &amp; Birds cage</li>
+        <li>Colourful Classrooms</li>
+        <li>Educational Field trips</li>
+        <li>CCTV Camera</li>
+        <li>Dinning Room</li>
+        <li>Ball Zone</li>
+        <li>Play area</li>
+        <li>Tinkering Minds Corner,</li>
+        <li>Story Corner</li>
+        <li>Day wise curriculum</li>
+      </ul>
+    </div>
+    <div className="map">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3905.1089673403944!2d75.50229016434433!3d11.82764144214496!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba4259f61926219%3A0xd570edebbe9d79c9!2sMambaram%2C%20Kerala%20670622!5e0!3m2!1sen!2sin!4v1586953806773!5m2!1sen!2sin" width="600" height="450" frameBorder="0" style={{'border':0}} allowFullscreen="" ariaHidden="false" tabIndex="0"></iframe>
+    </div>
+    <h1>Contact</h1>
+    <div className="sect">
+      <div className="contact">
+        <div className="grp">
+          <input type="text" placeholder="Name" />
+        </div>
+        <div className="grp">
+          <input type="text" placeholder="Email" />
+        </div>
+        <div className="grp">
+          <textarea placeholder="Your message"></textarea>
+        </div>
+        <div>
+          <button>Send!</button>
+        </div>
+      </div>
+    </div>
+    <h1>Gallery</h1>
+    <div className="sect">
+      <div className="gallery">
+        <div className="gt">
+          <img src="/photo1.jpg" />
+        </div>
+        <div className="gt">
+          <img src="/photo2.jpg" />
+        </div>
+        <div className="gt">
+          <img src="/photo3.jpg" />
+        </div>
+        <div className="gt">
+          <img src="/photo4.jpg" />
+        </div>
+        <div className="gt">
+          <img src="/photo5.jpg" />
+        </div>
+        <div className="gt">
+          <img src="/photo6.jpg" />
+        </div>
+      </div>
+    </div>
+  </div>
+)
